7387f74b9a7b37404d4ccd2e71451acfdf1b31db
Ginkko <Incubusalec@gmail.com>
2015-06-12 18:09:54 +0000

    Add support for editing questions.

--- controllers/question.js
+++ controllers/question.js
@@ -2,12 +2,31 @@ MessageBoard.QuestionController = Ember.ObjectController.extend({
   isEditing: false,
 
   actions: {
+
+    editQuestion: function() {
+
+      this.set('isEditing', true);
+
+    },
+
     deleteQuestion: function() {
-      
+
       if (confirm('Are you sure?')) {
         this.get('model').destroyRecord();
         this.transitionToRoute('questions');
       }
+    },
+
+    updateQuestion: function () {
+      this.get('model').destroyRecord();
+      var newQuestion = this.store.createRecord('question', {
+        title: this.get('title'),
+        user: this.get('user'),
+        text: this.get('text')
+      });
+      newQuestion.save();
+      this.set('isEditing', false);
+      this.transitionToRoute('questions');
     }
   }
 });
